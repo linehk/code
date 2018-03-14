508e99e876719993659081602df0090e19a8fb98
sulinehk <sulinehk@gmail.com>
2018-03-14 19:22:05 +0800

    add ch1

--- ch1/exercise1.12/another/main.go
+++ ch1/exercise1.12/another/main.go
@@ -8,8 +8,6 @@ import (
 	"io"
 	"math"
 	"math/rand"
-	"os"
-	"time"
 )
 
 // web
@@ -26,36 +24,28 @@ const (
 	blackIndex = 1 // next color in palette
 )
 
-var cycles = 5
-
 func main() {
 	// The sequence of image is deterministic unless we seed
 	// the pseudo-random number generator using the current time.
 	// Thanks to Randall McPherson for pointing out the omission.
-	rand.Seed(time.Now().UTC().UnixNano())
+	// rand.Seed(time.Now().UTC().UnixNano())
 
-	if len(os.Args) > 1 && os.Args[1] == "web" {
-		http.HandleFunc("/", handler)
-		/*
-			http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-				lissajous(w)
-			})
-		*/
-		log.Fatal(http.ListenAndServe("localhost:8000", nil))
-		return
-	}
-
-	lissajous(os.Stdout, cycles)
+	http.HandleFunc("/", handler)
+	log.Println("localhost:8000")
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
 }
 
-func lissajous(out io.Writer, cycles int) {
+func lissajous(out io.Writer, myCycles float64) {
 	const (
-		// cycles  = 5     // number of complete x oscillator revolutions
+		cycles  = 5     // number of complete x oscillator revolutions
 		res     = 0.001 // augular resolution
 		size    = 100   // image canvas covers [-size..+size]
 		nframes = 64    // number of animation frames
 		delay   = 8     // delay between frames in 10ms units
 	)
+	if myCycles == 0 {
+		myCycles = cycles
+	}
 
 	freq := rand.Float64() * 3.0 // relative frequency of y oscillator
 	anim := gif.GIF{LoopCount: nframes}
@@ -63,7 +53,7 @@ func lissajous(out io.Writer, cycles int) {
 	for i := 0; i < nframes; i++ {
 		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
 		img := image.NewPaletted(rect, palette)
-		for t := 0.0; t < float64(cycles)*2*math.Pi; t += res {
+		for t := 0.0; t < myCycles*2*math.Pi; t += res {
 			x := math.Sin(t)
 			y := math.Sin(t*freq + phase)
 			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5),
@@ -77,13 +67,15 @@ func lissajous(out io.Writer, cycles int) {
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	for k, v := range r.Form {
-		if k == "cycles" {
-			for _, w := range v {
-				s := w
-				cycles, _ = strconv.Atoi(s)
-			}
+	if err := r.ParseForm; err != nil {
+		log.Print(err)
+	}
+	if r.Form["cycles"] != nil {
+		cycles, err := strconv.Atoi(r.Form["cycles"][0])
+		if err != nil {
+			lissajous(w, float64(cycles))
 		}
 	}
-	lissajous(w, cycles)
+	lissajous(w, float64(0))
+
 }
