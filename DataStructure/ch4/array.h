a08902a2b8efba0eabf6444b922bc93f1bc57e98
sulinehk <sulinehk@gmail.com>
2018-04-03 21:26:59 +0800

    DataStructure/ch4: fix array.h

--- DataStructure/ch4/array.h
+++ DataStructure/ch4/array.h
@@ -3,5 +3,5 @@ typedef int datatype; // 假设数据元素的值为整型
 typedef struct {
         datatype *base; // 数组存储的首地址指针
         int index[3]; // 存放三维数组各维的长度
-        int c[3]; // 存放三维数组各维的 ci 值
+        int c[3]; // 存放三维数组各维的 c[i] 值
 } array;
