8bd374eb3c90bd7eda757c4fc461bb1c67931094
sulinehk <sulinehk@gmail.com>
2018-04-09 21:59:20 +0800

    delete LiaoxuefengJavaScript

--- LiaoxuefengJavaScript/koatest/start.js
+++
@@ -1,5 +0,0 @@
-'use strict'
-
-const app = require('./app');
-app.listen(3000);
-console.log('app start at port 3000...');
--- LiaoxuefengJavaScript/mocha-test/hello.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-module.exports = function (...rest) {
-    var sum = 0;
-    for (let n of rest) {
-        sum += n;
-    }
-    return sum;
-};
