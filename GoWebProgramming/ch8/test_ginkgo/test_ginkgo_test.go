a11e56ef4d166aaf7f96f84b8b2c56dcf0c1b36f
sulinehk <sulinehk@gmail.com>
2018-05-06 19:12:24 +0800

    GoWebProgramming: add ch8

---
+++ GoWebProgramming/ch8/test_ginkgo/test_ginkgo_test.go
@@ -0,0 +1,48 @@
+package main_test
+
+import (
+	. "ch8/test_ginkgo"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Get a post", func() {
+	var mux *http.ServeMux
+	var post *FakePost
+	var writer *httptest.ResponseRecorder
+
+	BeforeEach(func() {
+		post = &FakePost{}
+		mux = http.NewServeMux()
+		mux.HandleFunc("/post/", HandleRequest(post))
+		writer = httptest.NewRecorder()
+	})
+
+	Context("using an id", func() {
+		It("should get a post", func() {
+			request, _ := http.NewRequest("GET", "/post/1", nil)
+			mux.ServeHTTP(writer, request)
+
+			Expect(writer.Code).To(Equal(200))
+
+			var post Post
+			json.Unmarshal(writer.Body.Bytes(), &post)
+
+			Expect(post.Id).To(Equal(1))
+		})
+	})
+
+	Context("using a non-integer id", func() {
+		It("should get a HTTP 500 response", func() {
+			request, _ := http.NewRequest("GET", "/post/hello", nil)
+			mux.ServeHTTP(writer, request)
+
+			Expect(writer.Code).To(Equal(500))
+		})
+	})
+
+})
