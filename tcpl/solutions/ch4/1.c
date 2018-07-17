057578cfc618623e8cfcb08f239e2e22002c1c2b
sulinehk <sulinehk@gmail.com>
2018-07-17 20:21:32 +0800

    tcpl/solutions: add ch4

---
+++ tcpl/solutions/ch4/1.c
@@ -0,0 +1,29 @@
+/* strrindex: returns rightmost index of t in s, -1 if none */
+int strrindex(char s[], char t[])
+{
+        int i, j, k, pos;
+
+        pos = -1;
+        for (i = 0; s[i] != '\0'; i++) {
+                for (j=i, k=0; t[k]!='\0' && s[j]==t[k]; j++, k++)
+                        ;
+                if (k > 0 && t[k] == '\0')
+                        pos = i;
+        }
+        return pos;
+}
+
+#include <string.h>
+
+int strrindex(char s[], char t[])
+{
+        int i, j, k;
+
+        for (i = strlen(s) - strlen(t); i >= 0; i--) {
+                for (j=i, k=0; t[k]!='\0' && s[j]==t[k]; j++, k++)
+                        ;
+                if (k > 0 && t[k] == '\0')
+                        return i;
+        }
+        return -1;
+}
---
+++ tcpl/solutions/ch4/10.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <ctype.h>
+
+#define MAXLINE 10
+#define NUMBER '0'      /* signal that a number was found */
+
+int getline(char line[], int limit);
+
+int li = 0;     /* input line index */
+char line[MAXLINE];     /* ont input line */
+
+/* getop: get next operator or numeric operand */
+int getop(char s[])
+{
+        int c, i;
+
+        if (line[li] == '\0')
+                if (getline(line, MAXLINE) == 0)
+                        return EOF;
+                else
+                        li = 0;
+        while ((s[0] = c = line[li++]) == ' ' || c == '\t')
+                ;
+        s[1] = '\0';
+        if (!isdigit(c) && c != '.')
+                return c;       /* not a number */
+        i = 0;
+        if (isdigit(c)) /* collect integer part */
+                while (isdigit(s[++i] = c = line[li++]))
+                        ;
+        if (c == '.')   /* collect fraction part */
+                while (isdigit(s[++i] = c = line[li++]))
+                        ;
+        s[i] = '\0';
+        li--;
+        return NUMBER;
+}
