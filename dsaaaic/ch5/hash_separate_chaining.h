98d06a42b7e9ed965956b93e3fed27b4aa9279f1
sulinehk <sulinehk@gmail.com>
2018-09-13 21:02:27 +0800

    dsaaaic: add ch5

---
+++ dsaaaic/ch5/hash_separate_chaining.h
@@ -0,0 +1,14 @@
+typedef int ElementType;
+
+typedef unsigned int Index;
+
+struct ListNode;
+typedef struct ListNode *Position;
+struct HashTbl;
+typedef struct HashTbl *HashTable;
+
+HashTable InitializeTable(int TableSize);
+void DestroyTable(HashTable H);
+Position Find(ElementType Key, HashTable H);
+void Insert(ElementType Key, HashTable H);
+ElementType Retrieve(Position P);
