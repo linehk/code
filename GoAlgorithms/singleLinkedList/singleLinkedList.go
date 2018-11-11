11c7375d7bb5ec0061e4d0577d30ffb6ec40dfdc
sulinehk <sulinehk@gmail.com>
2018-11-11 12:12:40 +0800

    GoAlgorithms/singleLinkedList: delete unnecessary methods

--- GoAlgorithms/singleLinkedList/singleLinkedList.go
+++ GoAlgorithms/singleLinkedList/singleLinkedList.go
@@ -2,7 +2,6 @@
 package singleLinkedList
 
 import (
-	"errors"
 	"fmt"
 )
 
@@ -11,162 +10,41 @@ type node struct {
 	next  *node
 }
 
-type SingleLinkedList struct {
-	length int
-	head   *node
+type list struct {
+	head *node
 }
 
-func NewNode(v interface{}) *node {
-	return &node{v, nil}
+func New() *list {
+	l := new(list)
+	n := new(node)
+	n.value = nil
+	n.next = nil
+	l.head = n
+	return l
 }
 
-func (n node) GetValue() interface{} {
-	return n.value
+func (l *list) Insert(n, mark *node) {
+	n.next = mark.next
+	mark.next = n
 }
 
-func (n node) GetNext() *node {
-	return n.next
-}
-
-func NewSingleLinkedList() *SingleLinkedList {
-	return &SingleLinkedList{0, NewNode(nil)}
-}
-
-func InitSingleLinkedList(s []interface{}) (*SingleLinkedList, error) {
-	n := NewSingleLinkedList()
-	for _, v := range s {
-		err := n.InsertToTail(v)
-		if err != nil {
-			return nil, err
-		}
-	}
-	return n, nil
-}
-
-func (s SingleLinkedList) ToSlice() []interface{} {
-	slice := make([]interface{}, s.length)
-	cur := s.head.next
-	for i := range slice {
-		slice[i] = cur.GetValue()
-		cur = cur.next
-	}
-	return slice
-}
-
-// node must in the list
-func (s *SingleLinkedList) InsertAfter(n *node, v interface{}) error {
-	if n == nil {
-		return errors.New("node can't be nil")
-	}
-
-	newNode := NewNode(v)
-	newNode.next = n.next
-	n.next = newNode
-	s.length++
-	return nil
-}
-
-// node must in the list
-func (s *SingleLinkedList) InsertBefore(n *node, v interface{}) error {
-	if n == nil {
-		return errors.New("node can't be nil")
-	}
-	// can't insert before head node
-	if n == s.head {
-		return errors.New("can't insert before head")
-	}
-
-	pre := s.head
-	for pre.next != n {
-		pre = pre.next
-	}
-
-	newNode := NewNode(v)
-	newNode.next = pre.next
-	pre.next = newNode
-	s.length++
-	return nil
-}
-
-func (s *SingleLinkedList) InsertToHead(v interface{}) error {
-	return s.InsertAfter(s.head, v)
-}
-
-func (s *SingleLinkedList) InsertToTail(v interface{}) error {
-	cur := s.head
-	// cur.next != nil
-	// 1. prevent traversing a nil list
-	// 2. traversing list
-	for cur.next != nil {
-		cur = cur.next
-	}
-	return s.InsertAfter(cur, v)
-}
-
-func (s *SingleLinkedList) Delete(n *node) error {
-	if n == nil {
-		return errors.New("node can't be nil")
-	}
-	if n == s.head {
-		return errors.New("node can't be head node")
-	}
-
-	pre := s.head
+func (l *list) Delete(n *node) {
+	pre := l.head
 	for pre.next != n {
 		pre = pre.next
 	}
 
 	pre.next = n.next
-	n = nil
-	s.length--
-	return nil
 }
 
-func (s *SingleLinkedList) Reverse() {
-	var pre *node
-	cur := s.head.next
-	for cur != nil {
-		tmp := cur.next
-		cur.next = pre
-		pre = cur
-		cur = tmp
-	}
-	s.head.next = pre
-}
-
-func (s SingleLinkedList) Last() (*node, error) {
-	if s.head.next == nil {
-		return nil, errors.New("list can't be have 0 node")
-	}
-
-	last := s.head
-	for last.next != nil {
-		last = last.next
-	}
-	return last, nil
-}
-
-// got first effective node when index = 0
-func (s *SingleLinkedList) FindByIndex(index int) (*node, error) {
-	if index < 0 || index >= s.length {
-		return nil, errors.New("index can't be < 0 or >= s.length")
-	}
-
-	cur := s.head.next
-	for i := 0; i < index; i++ {
-		cur = cur.next
-	}
-	return cur, nil
-}
-
-func (s SingleLinkedList) Show() string {
-	cur := s.head.next
+func (l list) Show() string {
 	format := ""
-	for cur != nil {
-		format += fmt.Sprintf("%+v", cur.GetValue())
+	cur := l.head
+	for cur.next != nil {
 		cur = cur.next
-		if cur != nil {
-			format += "->"
+		format += fmt.Sprintf("%v", cur.value)
+		if cur.next != nil {
+			format += fmt.Sprint("->")
 		}
 	}
 	return format
