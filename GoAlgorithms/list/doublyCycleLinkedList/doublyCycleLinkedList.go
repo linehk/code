2a216df8a15306f315378fb8e65d956f58186fc3
sulinehk <sulinehk@gmail.com>
2018-11-12 21:07:14 +0800

    GoAlgorithms/list: add doublyCycleLinkedList

---
+++ GoAlgorithms/list/doublyCycleLinkedList/doublyCycleLinkedList.go
@@ -0,0 +1,49 @@
+package doublyCycleLinkedList
+
+import (
+        "fmt"
+)
+
+type node struct {
+        value interface{}
+        next *node
+        prev *node
+}
+
+type list struct {
+        head *node
+}
+
+func New() *list {
+        l := new(list)
+        l.head = new(node)
+        l.head.value = nil
+        l.head.next = l.head
+        l.head.prev = l.head
+        return l
+}
+
+func (l *list) Insert(n, mark *node) {
+        n.next = mark.next
+        n.prev = mark
+        mark.next.prev = n
+        mark.next = n
+}
+
+func (l *list) Delete(n *node) {
+        n.prev.next = n.next
+        n.next.prev = n.prev
+}
+
+func (l list) Show() string {
+        format := ""
+	cur := l.head
+	for cur.next != l.head {
+		cur = cur.next
+		format += fmt.Sprintf("%v", cur.value)
+		if cur.next != l.head {
+			format += fmt.Sprint("->")
+		}
+	}
+	return format
+}
---
+++ GoAlgorithms/list/doublyCycleLinkedList/doublyCycleLinkedList_test.go
@@ -0,0 +1,26 @@
+package doublyCycleLinkedList
+
+import (
+        "testing"
+)
+
+func TestDoublyCycleLinkedList(t *testing.T)  {
+        l := New()
+
+	// insert empty list
+	l.Insert(&node{3, nil, nil}, l.head)
+	t.Log(l.Show())
+
+	// delete only one node
+	l.Delete(l.head.next)
+	t.Log(l.Show())
+
+	l.Insert(&node{2, nil, nil}, l.head)
+	// insert after last node
+	l.Insert(&node{1, nil, nil}, l.head.next)
+	t.Log(l.Show())
+
+	// delete last node
+	l.Delete(l.head.next.next)
+	t.Log(l.Show())
+}
