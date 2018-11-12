aacdd63e0c40674fbf26063a7bca89a897c6be0c
sulinehk <sulinehk@gmail.com>
2018-11-12 20:40:36 +0800

    GoAlgorithms/list: add singleLinkedList and cycleSingleLinkedList tests

--- GoAlgorithms/list/singleLinkedList/singleLinkedList_test.go
+++ GoAlgorithms/list/singleLinkedList/singleLinkedList_test.go
@@ -6,16 +6,21 @@ import (
 
 func TestSingleLinkedList(t *testing.T) {
 	l := New()
+
+	// insert empty list
 	l.Insert(&node{3, nil}, l.head)
 	t.Log(l.Show())
 
+	// delete only one node
 	l.Delete(l.head.next)
 	t.Log(l.Show())
 
 	l.Insert(&node{2, nil}, l.head)
-	l.Insert(&node{1, nil}, l.head)
+	// insert after last node
+	l.Insert(&node{1, nil}, l.head.next)
 	t.Log(l.Show())
 
-	l.Delete(l.head.next)
+	// delete last node
+	l.Delete(l.head.next.next)
 	t.Log(l.Show())
 }
