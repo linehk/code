f1706bbd4a04eb7148ab2da406af7b5039d19bdc
sulinehk <sulinehk@gmail.com>
2018-11-17 22:19:55 +0800

    GoAlgorithms/DataStructures/stack: add linkedListStack

--- GoAlgorithms/DataStructures/stack/linkedListStack/stack.go
+++ GoAlgorithms/DataStructures/stack/linkedListStack/stack.go
@@ -1,1 +1,37 @@
 package linkedListStack
+
+type stack struct {
+	top *node
+}
+
+type node struct {
+	value interface{}
+	next  *node
+}
+
+func New() *stack {
+	s := new(stack)
+	return s
+}
+
+func (s *stack) Push(v interface{}) {
+	n := new(node)
+	n.value = v
+
+	// empty stack wouldn't execute this
+	// n.next will be nil
+	if s.top != nil {
+		n.next = s.top
+	}
+	s.top = n
+}
+
+func (s *stack) Pop() interface{} {
+	if s.top == nil {
+		return nil
+	}
+
+	v := s.top.value
+	s.top = s.top.next
+	return v
+}
--- GoAlgorithms/DataStructures/stack/linkedListStack/stack_test.go
+++ GoAlgorithms/DataStructures/stack/linkedListStack/stack_test.go
@@ -1,1 +1,18 @@
 package linkedListStack
+
+import (
+	"testing"
+)
+
+func TestStack(t *testing.T) {
+	s := New()
+	s.Push(1)
+	s.Push(2)
+	s.Push(3)
+
+	t.Log(s.Pop())
+	t.Log(s.Pop())
+	t.Log(s.Pop())
+
+	t.Log(s.Pop())
+}
