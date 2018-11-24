d4bdd68bba79eb32cd63f24a2f6368c1a6a164c3
sulinehk <sulinehk@gmail.com>
2018-11-24 21:53:52 +0800

    GoAlgorithms/DataStructures/queue: add linkedListQueue

--- GoAlgorithms/DataStructures/queue/linkedListQueue/queue.go
+++ GoAlgorithms/DataStructures/queue/linkedListQueue/queue.go
@@ -1,1 +1,47 @@
 package linkedListQueue
+
+type node struct {
+	value interface{}
+	next  *node
+}
+
+type queue struct {
+	len   int
+	front *node
+	rear  *node
+}
+
+func New() *queue {
+	n := new(node)
+	n.next = nil
+	n.value = nil
+
+	q := new(queue)
+	q.len = 0
+	q.front = n
+	q.rear = n
+	return q
+}
+
+func (q *queue) Enqueue(v interface{}) {
+	q.rear.value = v
+	n := new(node)
+	n.value = nil
+	n.next = nil
+	q.rear.next = n
+	q.rear = n
+	q.len++
+}
+
+func (q *queue) Dequeue() interface{} {
+	if q.front == q.rear {
+		return nil
+	}
+	v := q.front.value
+	q.front.value = nil
+	next := q.front.next
+	q.front.next = nil
+	q.front = next
+	q.len--
+	return v
+}
--- GoAlgorithms/DataStructures/queue/linkedListQueue/queue_test.go
+++ GoAlgorithms/DataStructures/queue/linkedListQueue/queue_test.go
@@ -1,1 +1,38 @@
 package linkedListQueue
+
+import (
+	"testing"
+)
+
+func TestQueue(t *testing.T) {
+	q := New()
+	q.Enqueue(1)
+	q.Enqueue(2)
+	q.Enqueue(3)
+	q.Enqueue(4)
+
+	t.Log(q.Dequeue())
+	t.Log(q.Dequeue())
+	t.Log(q.Dequeue())
+	t.Log(q.Dequeue())
+
+	q.Enqueue(1)
+	q.Enqueue(2)
+	q.Enqueue(3)
+	q.Enqueue(4)
+
+	t.Log(q.Dequeue())
+	t.Log(q.Dequeue())
+	t.Log(q.Dequeue())
+	t.Log(q.Dequeue())
+
+	q.Enqueue(1)
+	q.Enqueue(2)
+	q.Enqueue(3)
+	q.Enqueue(4)
+
+	t.Log(q.Dequeue())
+	t.Log(q.Dequeue())
+	t.Log(q.Dequeue())
+	t.Log(q.Dequeue())
+}
