af286fbcead1243c96965e5f8b53c51379b87e3d
sulinehk <sulinehk@gmail.com>
2018-12-04 16:18:22 +0800

    GoAlgorithms/DataStructures/array: use Table Drive Test

--- GoAlgorithms/DataStructures/array/array_test.go
+++ GoAlgorithms/DataStructures/array/array_test.go
@@ -7,73 +7,139 @@ import (
 )
 
 func TestGet(t *testing.T) {
-	a := New(1)
-	index := 0
-	want := 0
-	a.elements[index] = want
-	a.len++
-	got, err := a.Get(index)
-	if err != nil {
-		t.Error(err)
+	tests := []struct {
+		values []interface{}
+	}{
+		{[]interface{}{0, 1}},
 	}
-	if got != want {
-		t.Errorf("got %v, want %v", got, want)
+	for i, tt := range tests {
+		a := New(cap(tt.values))
+		for k, v := range tt.values {
+			a.elements[k] = v
+			a.len++
+		}
+
+		for k, v := range tt.values {
+			got, err := a.Get(k)
+			if err != nil {
+				t.Error(err)
+			}
+
+			if got != v {
+				t.Errorf("%v. got %v, want %v", i, got, v)
+			}
+		}
 	}
 }
 
 func TestAppend(t *testing.T) {
-	a := New(4)
-	want := []interface{}{0, 1, 2, 3}
-	for _, v := range want {
-		a.Append(v)
+	tests := []struct {
+		cap    int
+		values []interface{}
+	}{
+		{4, []interface{}{0, 1, 2, 3}},
+		{4, []interface{}{'a', 'b', 'c', 'd'}},
+		{2, []interface{}{0, 1, 2, 3}},
 	}
-	got := a.elements
-	if !utils.IsSameSlice(got, want) {
-		t.Errorf("want %v, got %v", want, got)
+	for i, tt := range tests {
+		a := New(tt.cap)
+
+		for _, v := range tt.values {
+			a.Append(v)
+		}
+
+		if !utils.IsSameSlice(a.elements, tt.values) {
+			t.Errorf("%v. got %v, want %v",
+				i, a.elements, tt.values)
+		}
 	}
 }
 
 func TestInsert(t *testing.T) {
-	a := New(4)
-	value := 2
-	slice := []interface{}{0, 1, 3}
-	want := []interface{}{0, 1, value, 3}
-	for _, v := range slice {
-		a.Append(v)
+	tests := []struct {
+		cap  int
+		give []interface{}
+		want []interface{}
+		i    int
+		v    interface{}
+	}{
+		{4, []interface{}{0, 1, 3}, []interface{}{0, 1, 2, 3}, 2, 2},
+		{4, []interface{}{'a', 'b', 'd'}, []interface{}{'a', 'b', 'c', 'd'}, 2, 'c'},
+		{2, []interface{}{0, 2}, []interface{}{0, 1, 2, nil}, 1, 1},
 	}
-	err := a.Insert(2, value)
-	if err != nil {
-		t.Error(err)
-	}
-	got := a.elements
-	if !utils.IsSameSlice(got, want) {
-		t.Errorf("want %v, got %v", want, got)
+	for i, tt := range tests {
+		a := New(tt.cap)
+		for k, v := range tt.give {
+			a.elements[k] = v
+			a.len++
+		}
+
+		err := a.Insert(tt.i, tt.v)
+		if err != nil {
+			t.Error(err)
+		}
+
+		if !utils.IsSameSlice(a.elements, tt.want) {
+			t.Errorf("%v. got %v, want %v", i, a.elements,
+				tt.want)
+		}
 	}
 }
 
 func TestDelete(t *testing.T) {
-	a := New(4)
-	slice := []interface{}{0, 1, 2, 3}
-	want := []interface{}{0, 1, 3, nil}
-	for _, v := range slice {
-		a.Append(v)
+	tests := []struct {
+		cap  int
+		give []interface{}
+		want []interface{}
+		i    int
+	}{
+		{4, []interface{}{0, 1, 2, 3}, []interface{}{0, 1, 3, nil}, 2},
+		{4, []interface{}{'a', 'b', 'c', 'd'}, []interface{}{'a', 'b', 'd', nil}, 2},
+		{8, []interface{}{0, 1, nil, nil, nil, nil, nil, nil}, []interface{}{0, nil, nil, nil}, 1},
 	}
-	err := a.Delete(2)
-	if err != nil {
-		t.Error(err)
-	}
-	got := a.elements
-	if !utils.IsSameSlice(got, want) {
-		t.Errorf("want %v, got %v", want, got)
+	for i, tt := range tests {
+		a := New(cap(tt.give))
+		for k, v := range tt.give {
+			a.elements[k] = v
+			if v != nil {
+				a.len++
+			}
+		}
+
+		err := a.Delete(tt.i)
+		if err != nil {
+			t.Error(err)
+		}
+
+		if !utils.IsSameSlice(a.elements, tt.want) {
+			t.Errorf("%v. got %v, want %v", i, a.elements,
+				tt.want)
+		}
 	}
 }
 
 func TestResize(t *testing.T) {
-	a := New(1)
-	want := cap(a.elements) * 2
-	a.resize(a.cap * 2)
-	got := cap(a.elements)
-	if got != want {
-		t.Errorf("want %v, got %v", want, got)
+	tests := []struct {
+		cap  int
+		give []interface{}
+		want []interface{}
+	}{
+		{4 * 2, []interface{}{0, 1, 2, 3}, []interface{}{0, 1, 2, 3, nil, nil, nil, nil}},
+		{8 / 2, []interface{}{0, 1, nil, nil, nil, nil, nil, nil}, []interface{}{0, 1, nil, nil}},
+	}
+	for i, tt := range tests {
+		a := New(cap(tt.give))
+		for k, v := range tt.give {
+			a.elements[k] = v
+			if v != nil {
+				a.len++
+			}
+		}
+
+		a.resize(tt.cap)
+
+		if !utils.IsSameSlice(a.elements, tt.want) {
+			t.Errorf("%v. got %v, want %v", i, a.elements, tt.want)
+		}
 	}
 }
