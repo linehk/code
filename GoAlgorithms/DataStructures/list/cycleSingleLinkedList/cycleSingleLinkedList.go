31700dcba8930f2f2c555fae9855727555b02f80
sulinehk <sulinehk@gmail.com>
2018-12-04 19:18:41 +0800

    GoAlgorithms/DataStructures/list/cycleSingleLinkedList: update

--- GoAlgorithms/DataStructures/list/cycleSingleLinkedList/cycleSingleLinkedList.go
+++ GoAlgorithms/DataStructures/list/cycleSingleLinkedList/cycleSingleLinkedList.go
@@ -1,33 +1,79 @@
-// CycleSingleLinkedList with head node
+// Package cycleSingleLinkedList implements cycle single linked-list with
+// a head node.
 package cycleSingleLinkedList
 
 import (
+	"errors"
 	"fmt"
 )
 
 type node struct {
 	value interface{}
 	next  *node
+	list  *list
 }
 
 type list struct {
 	head *node
 }
 
+// New creates a list.
 func New() *list {
 	l := new(list)
 	l.head = new(node)
 	l.head.value = nil
 	l.head.next = l.head
+	l.head.list = l
 	return l
 }
 
-func (l *list) Insert(n, mark *node) {
-	n.next = mark.next
-	mark.next = n
+// NewNode creates a node by v.
+func NewNode(v interface{}) *node {
+	n := new(node)
+	n.value = v
+	n.next = nil
+	n.list = nil
+	return n
 }
 
-func (l *list) Delete(n *node) {
+func (n node) Value() interface{} {
+	return n.value
+}
+
+func (n node) Next() *node {
+	return n.next
+}
+
+func (n node) List() *list {
+	return n.list
+}
+
+func (l list) Head() *node {
+	return l.head
+}
+
+// Insert insert n after mark.
+func (l *list) Insert(n, mark *node) error {
+	if mark.list != l {
+		return errors.New("node not in the list")
+	}
+
+	n.next = mark.next
+	mark.next = n
+	n.list = l
+	return nil
+}
+
+// Delete deletes n from list.
+func (l *list) Delete(n *node) error {
+	if n.list != l {
+		return errors.New("node not in the list")
+	}
+
+	if n == l.head {
+		return errors.New("can't delete head node")
+	}
+
 	pre := n
 	for pre.next != n {
 		pre = pre.next
@@ -37,9 +83,13 @@ func (l *list) Delete(n *node) {
 
 	n.value = nil
 	n.next = nil
+	n.list = nil
 	n = nil
+
+	return nil
 }
 
+// Show returns list friendly express.
 func (l list) Show() string {
 	format := ""
 	cur := l.head
