5dc3e5ef3da8b0f00d31d57c3121a686f5fed9ce
sulinehk <sulinehk@gmail.com>
2019-02-28 10:39:33 +0800

    TheGoProgrammingLanguage/ch1: fix exercise1.3

--- TheGoProgrammingLanguage/ch1/exercise1.3/echo_test.go
+++ TheGoProgrammingLanguage/ch1/exercise1.3/echo_test.go
@@ -2,25 +2,37 @@ package echo
 
 import (
 	"testing"
+	"time"
 )
 
-// 500000 3426 ns/op
+const n = 5000000
+
+// 5000000 4291 ns/op 21.458202917s
 func BenchmarkEcho1(b *testing.B) {
+	b.N = n
+	now := time.Now()
 	for i := 0; i < b.N; i++ {
 		Echo1()
 	}
+	b.Log(time.Since(now))
 }
 
-// 500000 3512 ns/op
+// 5000000 4630 ns/op 23.153483183s
 func BenchmarkEcho2(b *testing.B) {
+	b.N = n
+	now := time.Now()
 	for i := 0; i < b.N; i++ {
 		Echo2()
 	}
+	b.Log(time.Since(now))
 }
 
-// 500000 3419 ns/op
+// 5000000 4742 ns/op 23.710918847s
 func BenchmarkEcho3(b *testing.B) {
+	b.N = n
+	now := time.Now()
 	for i := 0; i < b.N; i++ {
 		Echo3()
 	}
+	b.Log(time.Since(now))
 }
