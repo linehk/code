ad01567d0b835d1747f2d327261f5ae256c01f0d
sulinehk <sulinehk@gmail.com>
2019-02-28 21:23:52 +0800

    TheGoProgrammingLanguage/ch1: fix exercise1.12

--- TheGoProgrammingLanguage/ch1/exercise1.12/main.go
+++ TheGoProgrammingLanguage/ch1/exercise1.12/main.go
@@ -2,13 +2,14 @@
 package main
 
 import (
+	"fmt"
 	"image"
 	"image/color"
 	"image/gif"
 	"io"
 	"math"
 	"math/rand"
-	"os"
+	"strconv"
 	"time"
 )
 
@@ -16,7 +17,6 @@ import (
 import (
 	"log"
 	"net/http"
-	"strconv"
 )
 
 var palette = []color.Color{color.White, color.Black}
@@ -26,36 +26,31 @@ const (
 	blackIndex = 1 // next color in palette
 )
 
-var cycles = 5
-
 func main() {
 	// The sequence of image is deterministic unless we seed
 	// the pseudo-random number generator using the current time.
 	// Thanks to Randall McPherson for pointing out the omission.
 	rand.Seed(time.Now().UTC().UnixNano())
 
-	if len(os.Args) > 1 && os.Args[1] == "web" {
-		http.HandleFunc("/", handler)
-		/*
-			http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-				lissajous(w)
-			})
-		*/
-		log.Fatal(http.ListenAndServe("localhost:8000", nil))
-		return
-	}
-
-	lissajous(os.Stdout, cycles)
+	http.HandleFunc("/", handler)
+	//http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	//	lissajous(w)
+	//})
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
 }
 
-func lissajous(out io.Writer, cycles int) {
+func lissajous(out io.Writer, customCycles float64) {
 	const (
-		// cycles  = 5     // number of complete x oscillator revolutions
-		res     = 0.001 // augular resolution
+		cycles  = 5     // number of complete x oscillator revolutions
+		res     = 0.001 // angular resolution
 		size    = 100   // image canvas covers [-size..+size]
 		nframes = 64    // number of animation frames
 		delay   = 8     // delay between frames in 10ms units
 	)
+
+	if customCycles == 0 {
+		customCycles = cycles
+	}
 
 	freq := rand.Float64() * 3.0 // relative frequency of y oscillator
 	anim := gif.GIF{LoopCount: nframes}
@@ -63,7 +58,7 @@ func lissajous(out io.Writer, cycles int) {
 	for i := 0; i < nframes; i++ {
 		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
 		img := image.NewPaletted(rect, palette)
-		for t := 0.0; t < float64(cycles)*2*math.Pi; t += res {
+		for t := 0.0; t < customCycles*2*math.Pi; t += res {
 			x := math.Sin(t)
 			y := math.Sin(t*freq + phase)
 			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5),
@@ -80,13 +75,15 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	if err := r.ParseForm(); err != nil {
 		log.Fatal(err)
 	}
-	for k, v := range r.Form {
-		if k == "cycles" {
-			for _, w := range v {
-				s := w
-				cycles, _ = strconv.Atoi(s)
-			}
-		}
+
+	if r.Form["cycles"] == nil {
+		lissajous(w, float64(0))
+		return
 	}
-	lissajous(w, cycles)
+
+	cycles, err := strconv.Atoi(r.Form["cycles"][0])
+	if err != nil {
+		fmt.Fprintf(w, "err: %v", err)
+	}
+	lissajous(w, float64(cycles))
 }
