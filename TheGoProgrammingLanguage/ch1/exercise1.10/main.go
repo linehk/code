5ff3b52bedc61f1a02b6045f4bef2c1529213c8d
sulinehk <sulinehk@gmail.com>
2019-03-01 08:59:35 +0800

    TheGoProgrammingLanguage/ch1: fix exercise1.10

--- TheGoProgrammingLanguage/ch1/exercise1.10/main.go
+++ TheGoProgrammingLanguage/ch1/exercise1.10/main.go
@@ -1,65 +1,40 @@
 // Fetchall fetches URLs in parallel and reports their times and sizes.
-// go run main.go http://www.baidu.com https://www.tmall.com > out
 package main
 
 import (
 	"fmt"
+	"io"
 	"io/ioutil"
 	"net/http"
 	"os"
 	"time"
 )
 
-type result struct {
-	data []byte
-	secs float64
-	err  error
-}
-
 func main() {
-	ch1 := make(chan result)
-	ch2 := make(chan result)
+	ch := make(chan string)
 	for _, url := range os.Args[1:] {
-		go timesFetch(url, ch1, ch2)
+		go fetchTwoTimes(url, ch)
 	}
-	for _, url := range os.Args[1:] {
-		fmt.Println(url)
-
-		res1 := <-ch1
-		res2 := <-ch2
-
-		fmt.Println("first response:")
-		fmt.Printf("%s\n", res1.data)
-		fmt.Println(res1.err)
-
-		fmt.Println("second response:")
-		fmt.Printf("%s\n", res2.data)
-		fmt.Println(res2.err)
-
-		fmt.Printf("first resp secs: %.2fs; second resp secs: %.2fs\n",
-			res1.secs, res2.secs)
+	for range os.Args[1:] {
+		fmt.Println(<-ch) // receive from channel ch
 	}
 }
 
-func timesFetch(url string, ch1, ch2 chan<- result) {
-	fetch(url, ch1)
-	fetch(url, ch2)
+func fetchTwoTimes(url string, ch chan<- string) {
+	ch <- "url: " + url + "\nresp1: " + fetch(url) + "\n" + "resp2: " + fetch(url)
 }
 
-func fetch(url string, ch chan<- result) {
+func fetch(url string) string {
 	start := time.Now()
 	resp, err := http.Get(url)
 	if err != nil {
-		ch <- result{err: err}
-		return
+		return fmt.Sprint(err)
 	}
-
-	data, err := ioutil.ReadAll(resp.Body)
+	nbytes, err := io.Copy(ioutil.Discard, resp.Body)
 	resp.Body.Close() // don't leak resources
 	if err != nil {
-		ch <- result{err: err}
-		return
+		return fmt.Sprintf("while reading %s: %v", url, err)
 	}
 	secs := time.Since(start).Seconds()
-	ch <- result{data: data, secs: secs, err: nil}
+	return fmt.Sprintf("%.2fs %7d", secs, nbytes)
 }
