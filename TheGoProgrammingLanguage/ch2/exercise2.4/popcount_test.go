d43b4bc974032837bc4bf77564d3193865bf3cf8
sulinehk <sulinehk@gmail.com>
2019-03-01 09:31:36 +0800

    TheGoProgrammingLanguage/ch2: fix exercise2.4

--- TheGoProgrammingLanguage/ch2/exercise2.4/popcount_test.go
+++ TheGoProgrammingLanguage/ch2/exercise2.4/popcount_test.go
@@ -4,27 +4,18 @@ import (
 	"testing"
 )
 
-func PopCountByShifting(x uint64) int {
-	n := 0
-	for i := uint(0); i < 64; i++ {
-		if x&(1<<i) != 0 {
-			n++
-		}
-	}
-	return 0
-}
+var x uint64 = 0x1234567890ABCDEF
 
-func BenchmarkPopCountByShifting(b *testing.B) {
+// BenchmarkPopCount-8 2000000000 0.32 ns/op
+func BenchmarkPopCount(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		PopCountByShifting(0x1234567890ABCDEF)
+		PopCount(x)
 	}
 }
 
-/*
-goos: linux
-goarch: amd64
-pkg: gopl/ch2/exercise2.4
-BenchmarkPopCountByShifting-8           50000000                29.5 ns/op
-PASS
-ok      gopl/ch2/exercise2.4       1.512s
-*/
+// BenchmarkPopCountShift-8 50000000 24.4 ns/op
+func BenchmarkPopCountShift(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		PopCountShift(x)
+	}
+}
