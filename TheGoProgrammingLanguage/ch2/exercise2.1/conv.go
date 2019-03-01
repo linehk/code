ef534c60e05dca5d54b991b52f713fce0f0ca1dc
sulinehk <sulinehk@gmail.com>
2019-03-01 09:14:19 +0800

    TheGoProgrammingLanguage/ch2/exercise2.1: add const

--- TheGoProgrammingLanguage/ch2/exercise2.1/conv.go
+++ TheGoProgrammingLanguage/ch2/exercise2.1/conv.go
@@ -3,13 +3,16 @@ package tempconv
 // CToF converts a Celsius temperature to Fahrenheit.
 func CToF(c Celsius) Fahrenheit { return Fahrenheit(c*9/5 + 32) }
 
-// FToC converts a Fahrenheit temperature to Celsius.
-func FToC(f Fahrenheit) Celsius { return Celsius((f - 32) * 5 / 9) }
-
 // CToK converts a Celsius temperature to Kelvin.
 func CToK(c Celsius) Kelvin { return Kelvin(c + 273.15) }
 
+// FToC converts a Fahrenheit temperature to Celsius.
+func FToC(f Fahrenheit) Celsius { return Celsius((f - 32) * 5 / 9) }
+
 // FToK converts a Fahrenheit temperature to Kelvin.
+//func FToK(f Fahrenheit) Kelvin { return CToK(FToC(f)) }
 func FToK(f Fahrenheit) Kelvin { return Kelvin((f + 459.67) * 5 / 9) }
 
-//func FToK(f Fahrenheit) Kelvin { return CToK(FToC(f)) }
+func KToC(k Kelvin) Celsius { return Celsius(k - 273.15) }
+
+func KToF(k Kelvin) Fahrenheit { return Fahrenheit(k*9/5 - 459.67) }
