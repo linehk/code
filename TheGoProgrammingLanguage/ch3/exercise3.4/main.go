8c243f503e5efac66a13b1bada546539c889e0ec
sulinehk <sulinehk@gmail.com>
2019-03-01 23:13:32 +0800

    TheGoProgrammingLanguage/ch3: fix exercise3.4

--- TheGoProgrammingLanguage/ch3/exercise3.4/main.go
+++ TheGoProgrammingLanguage/ch3/exercise3.4/main.go
@@ -6,15 +6,20 @@ import (
 	"log"
 	"math"
 	"net/http"
+	"strconv"
+)
+
+//var width, height = 600,320
+var (
+	width, height = 600, 320
+	xyscale       = width / 2 / xyrange
+	zscale        = float64(height) * 0.4
 )
 
 const (
-	width, height = 600, 320            // canvas size in pixels
-	cells         = 100                 // number of grid cells
-	xyrange       = 30.0                // axis ranges (-xyrange...+xyrange)
-	xyscale       = width / 2 / xyrange // pixels per x or y unit
-	zscale        = height * 0.4        // pixels per z unit
-	angle         = math.Pi / 6         // angle of x, y axes (=30°)
+	cells   = 100         // number of grid cells
+	xyrange = 30.0        // axis ranges (-xyrange...+xyrange)
+	angle   = math.Pi / 6 // angle of x, y axes (=30°)
 )
 
 var sin30, cos30 = math.Sin(angle), math.Cos(angle) // sin(30°), cos(30°)
@@ -26,6 +31,18 @@ func main() {
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "image/svg+xml")
+	if err := r.ParseForm(); err != nil {
+		log.Fatal(err)
+	}
+
+	if r.Form["width"] != nil {
+		width, _ = strconv.Atoi(r.Form["width"][0])
+	}
+
+	if r.Form["height"] != nil {
+		height, _ = strconv.Atoi(r.Form["height"][0])
+	}
+
 	fmt.Fprintf(w, "<svg xmlns='http://www.w3.org/2000/svg' "+
 		"style='stroke: grey; fill: white; stroke-width: 0.7' "+
 		"width='%d' height='%d'>", width, height)
@@ -39,7 +56,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 				ax, ay, bx, by, cx, cy, dx, dy)
 		}
 	}
-	fmt.Fprintf(w, "\n</svg>")
+	fmt.Fprintf(w, "</svg>\n")
 }
 func corner(i, j int) (float64, float64) {
 	// Find point (x,y) at corner of cell (i,j).
@@ -50,8 +67,8 @@ func corner(i, j int) (float64, float64) {
 	z := f(x, y)
 
 	// Project (x,y,z) isometrically onto 2-D SVG canvas (sx,xy).
-	sx := width/2 + (x-y)*cos30*xyscale
-	sy := height/2 + (x+y)*sin30*xyscale - z*zscale
+	sx := float64(width)/2 + (x-y)*cos30*float64(xyscale)
+	sy := float64(height)/2 + (x+y)*sin30*float64(xyscale) - z*zscale
 	return sx, sy
 }
 
