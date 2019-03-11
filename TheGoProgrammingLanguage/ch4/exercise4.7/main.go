1632317ffa9f7ad8e45d4fae773544554b243682
sulinehk <sulinehk@gmail.com>
2019-03-11 11:43:42 +0800

    TheGoProgrammingLanguage/ch4: fix exercise4.7

--- TheGoProgrammingLanguage/ch4/exercise4.7/main.go
+++ TheGoProgrammingLanguage/ch4/exercise4.7/main.go
@@ -2,18 +2,27 @@ package main
 
 import (
 	"fmt"
+	"unicode/utf8"
 )
 
 func main() {
-	b := []byte{'1', '2', '3'}
-	reverse(b)
-	for _, c := range b {
-		fmt.Printf("%q\n", c)
+	b := []byte("一 二 三")
+	reverseUTF8(b)
+	fmt.Printf("%s\n", b)
+}
+
+func reverseUTF8(b []byte) {
+	for i := 0; i < len(b); {
+		_, size := utf8.DecodeRune(b[i:])
+		reverse(b[i : i+size])
+		i += size
 	}
+	reverse(b)
 }
 
 func reverse(b []byte) {
-	for i, j := 0, len(b)-1; i < j; i, j = i+1, j-1 {
-		b[i], b[j] = b[j], b[i]
+	last := len(b) - 1
+	for i := 0; i < len(b)/2; i++ {
+		b[i], b[last-i] = b[last-i], b[i]
 	}
 }
