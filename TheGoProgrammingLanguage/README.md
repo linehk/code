c3177f17a1dd42226d5d4c5d25388d274b15bd8f
sulinehk <sulinehk@gmail.com>
2019-03-11 12:07:16 +0800

    move TheGoProgrammingLanguage

--- TheGoProgrammingLanguage/README.md
+++
@@ -1,3 +0,0 @@
-# Go 程序设计语言
-
-https://book.douban.com/subject/27044219/
--- TheGoProgrammingLanguage/ch1/helloworld/main.go
+++
@@ -1,8 +0,0 @@
-// Helloworld is our first Go program.
-package main
-
-import "fmt"
-
-func main() {
-	fmt.Println("Hello, 世界")
-}
